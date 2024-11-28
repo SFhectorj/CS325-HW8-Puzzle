fc04126014f28204d17bbd9adc8614b7f99664af
SFhectorj <SFhectorj@icloud.com>
2024-11-27 21:12:57 -0800

    hw8

--- Puzzle.py
+++ Puzzle.py
@@ -1,2 +1,9 @@
 def solve_puzzle(Board, Source, Destination):
+    """
+    :param Board: 2D list where '-' indicates empty cells and '#' indicates barriers.
+    :param Source: Tuple (a, b) representing the starting coordinates.
+    :param Destination: Tuple (x, y) representing the destination coordinates.
+    :returns: None if no valid path, otherwise returns a list of tuples representing path.
+    """
+    
     return
